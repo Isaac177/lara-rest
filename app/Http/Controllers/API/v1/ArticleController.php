e834453f438f757e92110be36a25cf6697ebc9cc
Isaac177 <isaacjogues177@gmail.com>
2022-11-07 22:43:12 +0600

    Creating API

---
+++ app/Http/Controllers/API/v1/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Article $article)
+    {
+        //
+    }
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
